6e09d94ecf9e919ad1562c58386b1387adab37b2
dollynho565 <77021968+dollynho565@users.noreply.github.com>
2021-01-10 10:19:23 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,45 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Lil $hiro:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+• ────── ✾ ────── •
+╔════════════════════
+  ➸DUVIDAS? 👇
+  WA.me/553897349613
+╚════════════════════
+
+╔════════════════════
+➸  NOS SIGA NO INSTAGRAM
+    https://instagram.com/bot_do_dolly?igshid=11oqpm0hqrow5
+╚════════════════════
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
+
